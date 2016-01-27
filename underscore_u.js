04e931d6fa371d49c6c360d071603f9982efd0ac
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2016-01-27 18:31:16 -0500

    Update underscore_u.js

--- underscore_u.js
+++ underscore_u.js
@@ -3,9 +3,6 @@
             var alog = function(text,label){
                     log((label ? label + ': ' : '') + text);
                 };
-                snapspin(r,d){
-                    return (~~(((~~r)/(~~d))+0.5)*(~~d))%360 + ((~~r) < 0 ? 360 : 0);
-                },
                 bound(value,lower,upper,integer){
                     v = Number(value)||~~value;
                     l = Number(lower)||~~lower;
@@ -13,10 +10,13 @@
                     r = integer ? ~~(Math.max(Math.min(v,u),l)) : Math.max(Math.min(v,u),l);
                     return r;
                 },
+                snapspin(r,d){
+                    return (~~(((~~r)/(~~d))+0.5)*(~~d))%360 + ((~~r) < 0 ? 360 : 0);
+                };
             return {
                 alog: alog,
-                snapspin: snapspin,
                 bound: bound
+                snapspin: snapspin
             };
             
         }()),
