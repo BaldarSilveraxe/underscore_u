d2f9651468fa7b8cbfe7b0b8cdcedb44ab998906
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2016-01-28 18:29:43 -0500

    Update underscore_u.js

--- underscore_u.js
+++ underscore_u.js
@@ -2,7 +2,8 @@
         _u = (function(){
             var alog = function(text,label){
                     log((label ? label + ': ' : '') + text);
-                };
+                },
+                
                 bound = function(value,lower,upper,integer){
                     var v = Number(value)||~~value,
                         l = Number(lower)||~~lower,
@@ -10,14 +11,39 @@
                         r = integer ? ~~(Math.max(Math.min(v,u),l)) : Math.max(Math.min(v,u),l);
                     return r;
                 },
+                
+                removeObj = function (obj,type){
+                    if(!_.isUndefined(obj)){
+                        if(_.has(obj,'remove') && _.isFunction(obj.remove) ){
+                            obj.remove();
+                            return true;
+                        }
+                        obj = getObj(type||'graphic',obj);
+                        if(obj){
+                            obj.remove();
+                            return true;
+                        }
+                    }
+                    return false;
+                },
+                
+                snapdist = function(distance,unit,integer){
+                    var d = Number(distance)||~~distance,
+                        u = Number(unit)||~~unit,
+                        r = integer ? ~~((d/u) * u) : ((d/u) * u);
+                    return r;
+                },
+                
                 snapspin = function(r,d){
-                    return (~~(((~~r)/(~~d))+0.5)*(~~d))%360 + ((~~r) < 0 ? 360 : 0);
+                   return (~~(((~~r)/(~~d))+0.5)*(~~d))%360 + ((~~r) < 0 ? 360 : 0);
                 };
+                
             return {
                 alog: alog,
-                bound: bound
-                snapspin: snapspin
+                bound: bound,
+                removeObj: removeObj,
+                snapspin: snapspin,
+                snapdist: snapdist
             };
-            
         }()),
 // ~~~> _u. utilities <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
